ba81fe0fed2a8c324e13a13801a76b9be24a854d
Chase Ohlson <chase@thenounproject.com>
2019-10-22 17:52:41 -0700

    https

--- src/util/endpoints.js
+++ src/util/endpoints.js
@@ -1,4 +1,4 @@
 export default {
   hello: '/.netlify/functions/hello',
-  users: 'http://5daf98b8f2946f001481d8ae.mockapi.io/users',
+  users: 'https://5daf98b8f2946f001481d8ae.mockapi.io/users',
 };
